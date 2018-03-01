c24e78956a5694ac2e47c697c81d0fb009cf5142
Karaggeorge <karaggeorge0@gmail.com>
2018-03-01 15:54:30 -0500

    Introduce xo and fix styling issues

--- cli.js
+++ cli.js
@@ -10,21 +10,21 @@ const tempfile = require('tempfile');
 const desktopIcons = require('hide-desktop-icons');
 
 const wallpapers = {
-  'cheetah': 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
-  'puma': 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
-  'jaguar': 'http://512pixels.net/downloads/macos-wallpapers/10-2.png',
-  'panther': 'http://512pixels.net/downloads/macos-wallpapers/10-3.png',
-  'tiger': 'http://512pixels.net/downloads/macos-wallpapers/10-4.png',
-  'leopard': 'http://512pixels.net/downloads/macos-wallpapers/10-5.png',
+  cheetah: 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
+  puma: 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
+  jaguar: 'http://512pixels.net/downloads/macos-wallpapers/10-2.png',
+  panther: 'http://512pixels.net/downloads/macos-wallpapers/10-3.png',
+  tiger: 'http://512pixels.net/downloads/macos-wallpapers/10-4.png',
+  leopard: 'http://512pixels.net/downloads/macos-wallpapers/10-5.png',
   'snow-leopard': 'http://512pixels.net/downloads/macos-wallpapers/10-6.png',
-  'lion': 'http://512pixels.net/downloads/macos-wallpapers/10-7.png',
+  lion: 'http://512pixels.net/downloads/macos-wallpapers/10-7.png',
   'mountain-lion': 'http://512pixels.net/downloads/macos-wallpapers/10-8.jpg',
-  'mavericks': 'http://512pixels.net/downloads/macos-wallpapers/10-9.jpg',
-  'yosemite': 'http://512pixels.net/downloads/macos-wallpapers/10-10.jpg',
+  mavericks: 'http://512pixels.net/downloads/macos-wallpapers/10-9.jpg',
+  yosemite: 'http://512pixels.net/downloads/macos-wallpapers/10-10.jpg',
   'el-capitan': 'http://512pixels.net/downloads/macos-wallpapers/10-11.jpg',
-  'sierra': 'http://512pixels.net/downloads/macos-wallpapers/10-12.jpg',
-  'high-sierra': 'https://512pixels.net/downloads/macos-wallpapers/10-13.jpg',
-}
+  sierra: 'http://512pixels.net/downloads/macos-wallpapers/10-12.jpg',
+  'high-sierra': 'https://512pixels.net/downloads/macos-wallpapers/10-13.jpg'
+};
 
 const cli = meow(`
   Usage
@@ -58,27 +58,27 @@ const cli = meow(`
 
 const stop = () => desktopIcons.forceShow().catch(() => {});
 
-const hideIcons = (path) => {
+const hideIcons = path => {
   stop().then(() => {
-    desktopIcons.hide(path, { detached: true }).then(pid => {
+    desktopIcons.hide(path, {detached: true}).then(pid => {
       fs.writeFile('hdi.pid', pid, () => process.exit(0));
     });
   });
-}
+};
 
-const getImage = (url) => new Promise(done => {
+const getImage = url => new Promise(resolve => {
   const file = tempfile(path.extname(url));
 
   got.stream(url)
-     .pipe(fs.createWriteStream(file))
-     .on('finish', () => {
-       done(file);
-     });
+    .pipe(fs.createWriteStream(file))
+    .on('finish', () => {
+      resolve(file);
+    });
 });
 
 const input = cli.input[0];
 
-if(!input) {
+if (!input) {
   hideIcons();
 } else if (input === 'versions') {
   Object.keys(wallpapers).forEach(key => console.log(key));
@@ -87,7 +87,7 @@ if(!input) {
 } else if (isUrl(input)) {
   getImage(input).then(path => hideIcons(path));
 } else if (fs.existsSync(input)) {
-  hideIcons(input)
+  hideIcons(input);
 } else if (wallpapers[input]) {
   getImage(wallpapers[input]).then(path => hideIcons(path));
 } else {
